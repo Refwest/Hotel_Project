dd2ff635d032ed3c54dcd91839eba77d281e462e
Filip Działa <fi.dziala@gmail.com>
2024-02-29 23:40:21 +0100

    Spring beginning

--- hotel-management/src/main/java/com/dzialahotels/hotelmanagement/HotelManagementApplication.java
+++ hotel-management/src/main/java/com/dzialahotels/hotelmanagement/HotelManagementApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelManagementApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(HotelManagementApplication.class, args);
 	}
