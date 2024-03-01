f0257464c10d3864acd2e83948ee1506a21f7826
Filip Działa <fi.dziala@gmail.com>
2024-03-01 22:01:50 +0100

    Spring Dependecies

--- hotel-management/src/main/java/com/dzialahotels/hotelmanagement/HotelManagementApplication.java
+++ hotel-management/src/main/java/com/dzialahotels/hotelmanagement/HotelManagementApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HotelManagementApplication {
-
-
-
 	public static void main(String[] args) {
-		SpringApplication.run(HotelManagementApplication.class, args);
+ 		SpringApplication.run(HotelManagementApplication.class, args);
 	}
 
 }
